374634d0cdf0d68595a66a27d960de1fc8e88e6b
tristanbrown <brown.tristan.r@gmail.com>
2021-12-07 04:39:55 -0800

    Get identical db entries by given fields

--- src/traitsgarden/db/query.py
+++ src/traitsgarden/db/query.py
@@ -14,3 +14,13 @@ def query_as_df(collection, queryargs=None):
     df = pdm.read_mongo(
         collection=collection, query=queryargs, db=db, index_col='_id')
     return df
+
+def get_existing(entity, fields):
+    """Find the existing entry(ies) in the database matching the given fields
+    on the given object.
+    """
+    model = entity.__class__
+    if not isinstance(fields, list):
+        fields = [fields]
+    keyfields = {field: getattr(entity, field) for field in fields}
+    return model.objects(**keyfields)
