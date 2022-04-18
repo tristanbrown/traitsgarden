9c9adc6ba99cedf0f18d3f31f9ec1d565163fd8b
tristanbrown <brown.tristan.r@gmail.com>
2022-04-18 02:44:00 -0700

    Generate plant_id as column_property

--- src/traitsgarden/db/models.py
+++ src/traitsgarden/db/models.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 from sqlalchemy import (Column, ForeignKey, asc, desc, Computed, UniqueConstraint,
-    ForeignKeyConstraint, cast, func
+    ForeignKeyConstraint, cast, func, select
 )
 from sqlalchemy.types import (Integer, TIMESTAMP, Numeric, String, Date,
     Boolean
@@ -33,23 +33,70 @@ class Cultivar(Base):
     hybrid = Column(Boolean(), default=False)
     description = Column(String())
 
+class Seeds(Base):
+    __tablename__ = 'seeds'
+
+    ## ID Fields
+    id = Column(Integer, primary_key=True)
+    cultivar_id = Column(Integer, ForeignKey('cultivar.id'), nullable=False)
+    cultivar = relationship("Cultivar")
+    name = association_proxy('cultivar', 'name')
+    category = association_proxy('cultivar', 'category')
+    year = Column(Integer(), nullable=False)
+    variant = Column(String(length=2), nullable=False)
+    pkt_id = column_property(
+        func.substr(cast(year, String), 3, 5) + variant)
+
+    __table_args__ = (
+        UniqueConstraint('cultivar_id', 'year', 'variant', name='_seeds_uc'),
+                     )
+
+    ## Parent/Child Relationships
+    mother_id = Column(Integer, ForeignKey('plant.id'))
+    father_id = Column(Integer, ForeignKey('plant.id'))
+    mother = relationship("Plant", foreign_keys=[mother_id], post_update=True)
+    father = relationship("Plant", foreign_keys=[father_id], post_update=True)
+
+    ## Seeds Data
+    source = Column(String(length=120))
+    last_count = Column(Integer())
+    generation = Column(String(length=2), default='1')
+    germination = Column(Numeric(2, 2))
+
+    def __repr__(self, recursion=False):
+        cultivar = self.cultivar
+        return f"<Seeds: {cultivar.name} - {cultivar.category} - {self.pkt_id}>"
+
+    @property
+    def db_obj(self):
+        existing = query_existing(self,
+            ['cultivar__name', 'cultivar__category', 'year', 'variant'])
+        if existing:
+            return existing[0]
+
 class Plant(Base):
     __tablename__ = 'plant'
 
+    ## ID Fields
     id = Column(Integer, primary_key=True)
     seeds_id = Column(Integer)
     seeds = relationship('Seeds', #foreign_keys=[seeds_id],
         primaryjoin="foreign(Plant.seeds_id)==Seeds.id"
         )
-    name = association_proxy('seeds', 'name')
-    category = association_proxy('seeds', 'category')
-    # seed_id = Column(String(length=4), nullable=False)
-    individual = Column(Integer(), nullable=False, default=1)
     ForeignKeyConstraint(
         ['seeds_id'], ['seeds.id'],
         name='fk_plant_seeds_id', use_alter=True
     )
+    name = association_proxy('seeds', 'name')
+    category = association_proxy('seeds', 'category')
+    individual = Column(Integer(), nullable=False, default=1)
+    plant_id = column_property(
+        select(Seeds.pkt_id
+            ).where(Seeds.id == seeds_id
+            ).scalar_subquery() \
+            + func.lpad(cast(individual, String), 2, '0'))
 
+    ## Plant Data
     start_date = Column(Date())
     germ_date = Column(Date())
     flower_date = Column(Date())
@@ -96,10 +143,6 @@ class Plant(Base):
             return 1
 
     @property
-    def plant_id(self):
-        return f"{self.parent_id}{str(self.individual).zfill(2)}"
-
-    @property
     def parent_seeds(self):
         """"""
         parent = Seeds.objects(
@@ -115,47 +158,6 @@ class Plant(Base):
     @property
     def db_obj(self):
         existing = query_existing(self, ['name', 'category', 'parent_id', 'individual'])
-        if existing:
-            return existing[0]
-
-class Seeds(Base):
-    __tablename__ = 'seeds'
-
-    ## ID Fields
-    id = Column(Integer, primary_key=True)
-    cultivar_id = Column(Integer, ForeignKey('cultivar.id'), nullable=False)
-    cultivar = relationship("Cultivar")
-    name = association_proxy('cultivar', 'name')
-    category = association_proxy('cultivar', 'category')
-    year = Column(Integer(), nullable=False)
-    variant = Column(String(length=2), nullable=False)
-    pkt_id = column_property(
-        func.substr(cast(year, String), 3, 5) + variant)
-
-    __table_args__ = (
-        UniqueConstraint('cultivar_id', 'year', 'variant', name='_seeds_uc'),
-                     )
-
-    ## Parent/Child Relationships
-    mother_id = Column(Integer, ForeignKey('plant.id'))
-    father_id = Column(Integer, ForeignKey('plant.id'))
-    mother = relationship("Plant", foreign_keys=[mother_id], post_update=True)
-    father = relationship("Plant", foreign_keys=[father_id], post_update=True)
-
-    ## Seeds Data
-    source = Column(String(length=120))
-    last_count = Column(Integer())
-    generation = Column(String(length=2), default='1')
-    germination = Column(Numeric(2, 2))
-
-    def __repr__(self, recursion=False):
-        cultivar = self.cultivar
-        return f"<Seeds: {cultivar.name} - {cultivar.category} - {self.pkt_id}>"
-
-    @property
-    def db_obj(self):
-        existing = query_existing(self, 
-            ['cultivar__name', 'cultivar__category', 'year', 'variant'])
         if existing:
             return existing[0]
 
