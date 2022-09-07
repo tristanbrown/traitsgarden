3a5d6948f1165fd17c87587d067a6ddc3c2088ba
tristanbrown <brown.tristan.r@gmail.com>
2022-09-06 23:34:16 -0700

    Navbar on all pages

--- src/traitsgarden/__init__.py
+++ src/traitsgarden/__init__.py
@@ -6,6 +6,7 @@ from dash_bootstrap_components._components.Container import Container
 
 from traitsgarden.settings import Config
 from traitsgarden.db.connect import Session, connect_db
+from traitsgarden.fragments.navbar import navbar
 
 external_stylesheets = [dbc.themes.COSMO]
 app = Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
@@ -15,6 +16,7 @@ app.config.suppress_callback_exceptions = True
 
 ## Initial layout
 app.layout = Container(html.Div([
+    navbar,
     page_container,
     html.Br(),
     html.Div([
--- src/traitsgarden/pages/details.py
+++ src/traitsgarden/pages/details.py
@@ -20,7 +20,6 @@ def layout(cultivarid=None, seedsid=None, plantid=None):
             session, cultivarid, seedsid, plantid
         )
     return html.Div([
-        navbar,
         dcc.Store(id='ids', data=ids),
         section1,
         html.Br(),
@@ -133,60 +132,3 @@ def save_changes(n_clicks, ids, input_fields, inputs,
                 setattr(obj, field, val)
                 updates[field] = val
     return f"Changes Saved: {updates}"
-
-PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
-
-search_bar = dbc.Row(
-    [
-        dbc.Col(dbc.Input(type="search", placeholder="Search")),
-        dbc.Col(
-            dbc.Button(
-                "Search", color="primary", className="ms-2", n_clicks=0
-            ),
-            width="auto",
-        ),
-    ],
-    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
-    align="center",
-)
-
-navbar = dbc.Navbar(
-    # dbc.Container(
-        [
-            html.A(
-                # Use row and col to control vertical alignment of logo / brand
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Navbar", className="ms-2")),
-                    ],
-                    align="center",
-                    className="g-0",
-                ),
-                href="https://plotly.com",
-                style={"textDecoration": "none"},
-            ),
-            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
-            dbc.Collapse(
-                search_bar,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True,
-            ),
-        ],
-    # ),
-    color="dark",
-    dark=True,
-)
-
-
-# add callback for toggling the collapse on small screens
-@callback(
-    Output("navbar-collapse", "is_open"),
-    [Input("navbar-toggler", "n_clicks")],
-    [State("navbar-collapse", "is_open")],
-)
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
