5613f816d8ed3024ff92173422f71a7605789650
tristanbrown <brown.tristan.r@gmail.com>
2022-09-14 03:01:07 -0700

    Make seeds table display flexible

--- src/traitsgarden/pages/seedstable.py
+++ src/traitsgarden/pages/seedstable.py
@@ -14,6 +14,11 @@ def layout(**kwargs):
         dash_table.DataTable(
             id='seeds-table',
             editable=True,
+            hidden_columns=['cultivar_id'],
+            filter_action='native',
+            filter_options={'case': 'insensitive'},
+            sort_action='native',
+            markdown_options={'link_target': '_self'},
         ),
     ])
 
@@ -27,8 +32,24 @@ def update_content(timestamp, rows):
 
     if df is None:
         return [{}], []
-    return df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]
+    cols = [{"name": i, "id": i, 'hideable': True} for i in df.columns]
+    for col in cols:
+        if col['name'] in ['id', 'name']:
+            col['presentation'] = 'markdown'
+    return (
+        df.to_dict('records'),
+        cols
+    )
 
 def get_seeds_data():
     with Session.begin() as session:
-        return query_as_df('seeds')
+        query = """SELECT b.name, b.category, a.*
+            FROM seeds a
+            JOIN cultivar b
+            ON a.cultivar_id = b.id
+            """
+        df = query_as_df(query)
+    df = df.set_index(['id', 'cultivar_id']).reset_index()
+    df['name'] = df.apply(lambda row: f"[{row['name']}](details?cultivarid={row['cultivar_id']})", axis=1)
+    df['id'] = df.apply(lambda row: f"[{row['id']}](details?seedsid={row['id']})", axis=1)
+    return df
