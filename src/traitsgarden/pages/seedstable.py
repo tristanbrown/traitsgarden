087c26f949429d053ce5d9760cd490dfe258fec4
tristanbrown <brown.tristan.r@gmail.com>
2022-09-14 01:30:31 -0700

    Populate seeds table in app

--- src/traitsgarden/pages/seedstable.py
+++ src/traitsgarden/pages/seedstable.py
@@ -3,31 +3,32 @@ from dash import dcc, html, callback, register_page, dash_table
 from dash.dependencies import Input, Output, State, MATCH, ALL
 import dash_bootstrap_components as dbc
 from traitsgarden.db.connect import Session
+from traitsgarden.db.query import query_as_df
 from traitsgarden.db.models import Plant, Seeds, Cultivar
 
 register_page(__name__, path='/traitsgarden/seedstable')
 
-layout = html.Div([
-    html.Br(),
-    dash_table.DataTable(
-        id='computed-table',
-        columns=[
-            {'name': 'Input Data', 'id': 'input-data'},
-            {'name': 'Input Squared', 'id': 'output-data'}
-        ],
-        data=[{'input-data': i} for i in range(11)],
-        editable=True,
-    ),
-])
+def layout(**kwargs):
+    return html.Div([
+        html.Br(),
+        dash_table.DataTable(
+            id='seeds-table',
+            editable=True,
+        ),
+    ])
 
 @callback(
-    Output('computed-table', 'data'),
-    Input('computed-table', 'data_timestamp'),
-    State('computed-table', 'data'))
-def update_columns(timestamp, rows):
-    for row in rows:
-        try:
-            row['output-data'] = float(row['input-data']) ** 2
-        except:
-            row['output-data'] = 'NA'
-    return rows
+    Output('seeds-table', 'data'),
+    Output('seeds-table', 'columns'),
+    Input('seeds-table', 'data_timestamp'),
+    State('seeds-table', 'data'))
+def update_content(timestamp, rows):
+    df = get_seeds_data()
+
+    if df is None:
+        return [{}], []
+    return df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]
+
+def get_seeds_data():
+    with Session.begin() as session:
+        return query_as_df('seeds')
