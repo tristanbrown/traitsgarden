9d7a06b9e2522c91aca4bacaa9f16a4a7104926c
tristanbrown <brown.tristan.r@gmail.com>
2022-09-19 04:32:50 -0700

    Base alembic config on single DB version

--- migrations/env.py
+++ migrations/env.py
@@ -1,15 +1,11 @@
 import os
-import logging
 from logging.config import fileConfig
-import re
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 from traitsgarden.db.connect import Base
-
-USE_TWOPHASE = False
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,37 +15,22 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-logger = logging.getLogger("alembic.env")
 
-# gather section names referring to different
-# databases.  These are named "engine1", "engine2"
-# in the sample .ini file.
-db_names = config.get_main_option("databases")
-
-db_name = config.config_ini_section # active config ini section is the db name that we have chosen
-config.set_main_option(
-    "sqlalchemy.url",
-    f"postgresql://postgres:{os.getenv('POSTGRES_PASSWORD')}@postgres/{db_name}"
-)
-
-# add your model's MetaData objects here
-# for 'autogenerate' support.  These must be set
-# up to hold just those tables targeting a
-# particular database. table.tometadata() may be
-# helpful here in case a "copy" of
-# a MetaData is needed.
+# add your model's MetaData object here
+# for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = {
-#       'engine1':mymodel.metadata1,
-#       'engine2':mymodel.metadata2
-# }
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+db_name = config.config_ini_section # active config ini section is the db name that we have chosen
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql://postgres:{os.getenv('POSTGRES_PASSWORD')}@postgres/{db_name}"
+)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -63,28 +44,16 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    # for the --sql use case, run migrations for each URL into
-    # individual files.
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
 
-    engines = {}
-    for name in re.split(r",\s*", db_names):
-        engines[name] = rec = {}
-        rec["url"] = context.config.get_section_option(name, "sqlalchemy.url")
-
-    for name, rec in engines.items():
-        logger.info("Migrating database %s" % name)
-        file_ = "%s.sql" % name
-        logger.info("Writing output to %s" % file_)
-        with open(file_, "w") as buffer:
-            context.configure(
-                url=rec["url"],
-                output_buffer=buffer,
-                target_metadata=target_metadata.get(name),
-                literal_binds=True,
-                dialect_opts={"paramstyle": "named"},
-            )
-            with context.begin_transaction():
-                context.run_migrations(engine_name=name)
+    with context.begin_transaction():
+        context.run_migrations()
 
 
 def run_migrations_online() -> None:
